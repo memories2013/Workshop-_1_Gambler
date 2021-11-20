ba1fbc061c80c8f1da1312c4a0df5e8f9399627a
memories2013 <nshivaprasad41@gmail.com>
2021-11-20 19:57:33 +0530

    Adding Method and loops

--- src/com/gambling/Gambling.java
+++ src/com/gambling/Gambling.java
@@ -4,22 +4,25 @@ import java.util.Random;
 
 public class Gambling {
 	static int stack=100;  
-	final static int BET=1;    
+	final static int BET=1;
 	public void Game() {
 		System.out.println("Welcome To Gambler Simulator");
 		System.out.println("Every Day Stack: "+stack);
-		System.out.println("Bet Price: "+BET);
+		System.out.println("Beat Price: "+BET);
 		
+		int stack_Upper = 150;  
+		int stack_Lower =50;    
+		
+		while(stack > stack_Lower && stack < stack_Upper) {   
 		Random random = new Random();  
-		int Bet = random.nextInt(2);
-		
-		if(Bet == 1) {
+		int bet = random.nextInt(2);
+		if(bet == 1) {
 			System.out.println("Bet Win");
 			System.out.println("Final Amount: "+(stack += BET)); 
 		}else {
 			System.out.println("Bet Loss");
 			System.out.println("Final Amount: "+(stack -= BET)); 
 		}
-		
+		}
 	}
 }
