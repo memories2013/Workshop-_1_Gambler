4a8b939522a3748a806c7ed71ce70c1455c82dfd
memories2013 <nshivaprasad41@gmail.com>
2021-11-20 19:42:20 +0530

    Player win or lose the bet

--- src/com/gambling/Gambling.java
+++ src/com/gambling/Gambling.java
@@ -1,11 +1,25 @@
 package com.gambling;
 
+import java.util.Random;
+
 public class Gambling {
-	public void game() {
-		System.out.println("Game Starts");
-		int stack=100;
-		int BET=1;
+	static int stack=100;  
+	final static int BET=1;    
+	public void Game() {
+		System.out.println("Welcome To Gambler Simulator");
 		System.out.println("Every Day Stack: "+stack);
 		System.out.println("Bet Price: "+BET);
+		
+		Random random = new Random();  
+		int Bet = random.nextInt(2);
+		
+		if(Bet == 1) {
+			System.out.println("Bet Win");
+			System.out.println("Final Amount: "+(stack += BET)); 
+		}else {
+			System.out.println("Bet Loss");
+			System.out.println("Final Amount: "+(stack -= BET)); 
+		}
+		
 	}
 }
--- src/com/gambling/TestGambling.java
+++ src/com/gambling/TestGambling.java
@@ -3,6 +3,6 @@ package com.gambling;
 public class TestGambling {
 	public static void main(String[] args) {
 		Gambling gambler = new Gambling();
-		gambler.game();
+		gambler.Game();
 	}
 }
