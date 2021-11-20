00761d75489d2a2be1019767eadb9cfaf291db05
memories2013 <nshivaprasad41@gmail.com>
2021-11-20 19:27:46 +0530

    Game Starts

---
+++ src/com/gambling/Gambling.java
@@ -0,0 +1,11 @@
+package com.gambling;
+
+public class Gambling {
+	public void game() {
+		System.out.println("Game Starts");
+		int stack=100;
+		int BET=1;
+		System.out.println("Every Day Stack: "+stack);
+		System.out.println("Bet Price: "+BET);
+	}
+}
---
+++ src/com/gambling/TestGambling.java
@@ -0,0 +1,8 @@
+package com.gambling;
+
+public class TestGambling {
+	public static void main(String[] args) {
+		Gambling gambler = new Gambling();
+		gambler.game();
+	}
+}
